0ba49ead20b8fe4cd449f02f132248a53f80fe4f
MrGamePLAY1 <47218530+mrgameplay1@users.noreply.github.com>
2020-04-26 03:46:39 +0100

    Working navigation to player creation on navbar

--- app/src/main/java/com/example/smanagement/MainActivity.java
+++ app/src/main/java/com/example/smanagement/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -14,7 +15,8 @@ public class MainActivity extends AppCompatActivity {
     //variables
     TextView managerLogout;
 
-
+    //buttons (navbar)
+    Button homeBtn, statsBtn, checklistBtn, calenderBtn, messagingBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
 
         //setting views
         managerLogout = findViewById(R.id.logout);
+        homeBtn = findViewById(R.id.homeBtn);
+        statsBtn = findViewById(R.id.statsBtn);
+        checklistBtn = findViewById(R.id.checklistBtn);
+        calenderBtn = findViewById(R.id.calenderBtn);
+        messagingBtn = findViewById(R.id.messagingBtn);
 
         managerLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,5 +39,16 @@ public class MainActivity extends AppCompatActivity {
                     finish();
             }
         });
+
+        //send to creation of player
+        statsBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, playerCreation.class));
+            }
+        });
+
+
     }
 }
